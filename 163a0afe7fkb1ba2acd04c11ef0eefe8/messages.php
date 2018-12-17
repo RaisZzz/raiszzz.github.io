55ba28d3aa4dc34de408e124e9244073b93052ea
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2018-12-18 00:27:50 +0300

    Add files via upload

---
+++ 163a0afe7fkb1ba2acd04c11ef0eefe8/messages.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+	  <meta charset="UTF-8">
+	  <title>Заявка</title>
+
+	  <style>
+		body{
+		  	display: -ms-flexbox;
+			display: flex;
+			-ms-flex-flow: row wrap;
+			flex-flow: row wrap;
+			margin: 0;
+			padding: 0;
+		}
+	  	.man{
+	  		padding: 0 15px;
+	  		width: 50%;
+	  		box-sizing: border-box;
+    		box-shadow: 1px 1px 0px rgba(51, 51, 51, 0.2);
+	  	}
+	  </style>
+	</head>
+
+	<body>
+
+		<?php 
+			include "mans.php";
+		?>
+
+	</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!-- saved from url=(0036)#page5 -->
+<html lang="ru" class="mdl-js">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <title>Главная страница</title>
+      <meta name="viewport" content="width=device-width">
+      <script>(function(){setVP();window.addEventListener("orientationchange", setVP, false);function setVP() {var w = (!window.orientation || window.orientation == 180) ? screen.width : screen.height;var v = (w >= 768 && w <= 1024) ? '1024' : 'device-width';document.head.querySelector("[name=viewport]").setAttribute("content","width=" + v);}}());</script>
+      <meta property="og:type" content="website">
+      <meta property="og:title" content="Главная страница">
+      <meta property="og:description" content="">
+      <link id="_core.css" href="./index_files/_core.css" rel="stylesheet" type="text/css">
+      <link id="_components.css" href="./index_files/_components.css" rel="stylesheet" type="text/css">
+      <link id="_adaptive.css" href="./index_files/_adaptive.css" rel="stylesheet" type="text/css">
+      <link id="_fixes.css" href="./index_files/_fixes.css" rel="stylesheet" type="text/css">
+      <link id="528935_1544948835.css" href="./index_files/528935_1544948835.css" rel="stylesheet" type="text/css">
+      <style type="text/css">/* Chart.js */
+         @-webkit-keyframes chartjs-render-animation{from{opacity:0.99}to{opacity:1}}
+         @keyframes chartjs-render-animation{from{opacity:0.99} to {opacity:1}}
+         .chartjs-render-monitor{-webkit-animation:chartjs-render-animation 0.001s; animation:chartjs-render-animation 0.001s;}
+      </style>
+   </head>
+   <body class="adaptive ready is-pointer is-view" id="body-content">
+   	
+    	<?php
+			include 'content.html';
+    	?>
+
+
+      <link class="font-setup-import" rel="stylesheet" href="./index_files/css">
+      <style class="font-setup">body .text-style-title-main { font-family: "Yeseva One", Helvetica, Verdana, sans-serif }
+         body .text-style-title { font-family: Roboto, Helvetica, Verdana, sans-serif }
+         body .text-style-subtitle { font-family: Roboto, Helvetica, Verdana, sans-serif }
+         body { font-family: Roboto, Helvetica, Verdana, sans-serif !important }
+         body .text-style-quote { font-family: "Playfair Display", Helvetica, Verdana, sans-serif }
+      </style>
+      <script src="./index_files/jquery-3.2.1+debounce.min.js" type="text/javascript"></script>
+      <script defer="" src="./index_files/_core.js" type="text/javascript"></script>
+      <script defer="" src="./index_files/_client.js" type="text/javascript"></script>
+      <script id="flexbe-vars">
+         if (typeof flexbe_cli !== 'object') flexbe_cli = {};
+         flexbe_cli.s_id = 422290;
+         flexbe_cli.group_id = 249022;
+         flexbe_cli.p_id = 528935;
+         flexbe_cli.p_type = 0;
+         flexbe_cli.theme_id = 4;
+         flexbe_cli.is_admin = false;
+         flexbe_cli.adaptive = true;
+         flexbe_cli.yandex_id = 'undefined';
+         flexbe_cli.google_id = 'undefined';
+         flexbe_cli.google_maps_api_key = 'undefined';
+      </script>
+      <script async="" src="./index_files/imask.min.js"></script>
+      <script async="" src="./index_files/anime.min.js"></script>
+   </body>
+</html>
+<script async="" src="./index_files/imask.min.js.Без названия"></script><script async="" src="./index_files/anime.min.js.Без названия"></script></body></html>
