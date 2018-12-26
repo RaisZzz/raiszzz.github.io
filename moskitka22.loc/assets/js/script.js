a70accbf2d014e8218bb991ef2cb7ede2c38bf76
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2018-12-26 23:45:53 +0300

    Add files via upload

---
+++ moskitka22.loc/assets/js/script.js
@@ -0,0 +1,8 @@
+function openmenu() {
+	document.getElementById("mobile-nav").classList.toggle('mobile-menu-active');
+	document.getElementById("body").classList.toggle('body-overflow');
+}
+function submenu() {
+	document.getElementById('submenu-mobile').classList.toggle('submenu-active');
+	document.getElementById('li-sub').classList.toggle('li-sub-active');
+}
