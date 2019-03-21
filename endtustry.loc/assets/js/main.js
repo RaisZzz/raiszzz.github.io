e196bfec1c6d6508d6ca5854fc8d9eb8c861a1d5
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-03-22 01:06:35 +0300

    Add files via upload

--- endtustry.loc/assets/js/main.js
+++ endtustry.loc/assets/js/main.js
@@ -1,8 +1,7 @@
-btn = document.getElementById('submenu');
-$('#menu-btn').click(function toggle(el) {
-	btn.style.opacity = (btn.style.opacity == '0') ? '1' : '0';
-	btn.style.display = (btn.style.display == 'none') ? 'flex' : 'none';
+$('#menu-btn').click(function() {
+  $('#submenu').toggleClass('show-menu');
 })
+
 $('.submenu-li1').click(function(){
 	$('.submenu-2').removeClass('visible-div');
 	$(this).find('.submenu-2').addClass('visible-div');
