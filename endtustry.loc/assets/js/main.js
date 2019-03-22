5702125d63af975158ad636ec9623b1cf3768aa1
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-03-22 10:52:55 +0300

    Add files via upload

--- endtustry.loc/assets/js/main.js
+++ endtustry.loc/assets/js/main.js
@@ -6,3 +6,20 @@ $('.submenu-li1').click(function(){
 	$('.submenu-2').removeClass('visible-div');
 	$(this).find('.submenu-2').addClass('visible-div');
 });
+
+$('#slider-2').on('afterChange', function(){
+	count = document.getElementById("counter");
+	text = count.textContent;
+	if(text == '01'){
+		count.innerHTML = '02';
+	}
+	else if(text == '02'){
+		count.innerHTML = '03'
+	}
+	else if(text == '03'){
+		count.innerHTML = '04'
+	}
+	else if(text == '04'){
+		count.innerHTML = '01'
+	}
+})
