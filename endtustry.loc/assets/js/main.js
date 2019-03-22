0f06632a632927520d5e2788ef08673c5390c60b
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-03-22 13:42:50 +0300

    Add files via upload

--- endtustry.loc/assets/js/main.js
+++ endtustry.loc/assets/js/main.js
@@ -1,3 +1,10 @@
+$(window).on('load', function () {
+	$preloader = $('.loaderArea'),
+	$loader = $preloader.find('.loader');
+	$loader.fadeOut();
+	$preloader.delay(350).fadeOut('slow');
+});
+
 $('#menu-btn').click(function() {
   $('#submenu').toggleClass('show-menu');
 })
