1257ce426ddadad1dc1905b99cb0dff1a8b50fcf
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-04-23 16:45:04 +0300

    Add files via upload

--- head.loc/assets/js/main.js
+++ head.loc/assets/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	$('.step').click(function(){
 		$(this).find('.sub-step').toggleClass('visible');
-	})
+	});
+	$('.popto').click(function(){
+		$($(this).attr('href')).toggleClass('popup-visible');
+		$('.overlay').toggleClass('overlay-visible');
+	});
+	$('.close').click(function(){
+		$('.overlay').toggleClass('overlay-visible');
+		$('.popup').toggleClass('popup-visible')
+	});
 });
