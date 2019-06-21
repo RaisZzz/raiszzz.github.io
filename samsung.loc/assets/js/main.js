69373fd5a7f72b09020d05ca4e503f82c192f895
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-06-21 15:22:34 +0300

    Add files via upload

---
+++ samsung.loc/assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		centerMode: true,
+		centerPadding: '0px',
+		slidesToShow: 3,
+		prevArrow: $('#left-arrow'),
+		nextArrow: $('#right-arrow')
+	});
+});
