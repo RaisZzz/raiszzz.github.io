f6b404fa7ea94dab2f089231e34a4d18f117e41c
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-06-22 14:56:04 +0300

    Add files via upload

--- samsung.loc/assets/js/main.js
+++ samsung.loc/assets/js/main.js
@@ -1,3 +1,9 @@
+// 3) Блок "10 лет впечатлений". Фотогалерея #снятонаGalaxy.
+// Галерея будет двигаться при прокручивании колесика мыши 
+// и при перетягивании мыши (больше, чем на одну позицию).
+// 4) Блок "10 лет технологий"
+// Обзоры и материалы, которые касаются только устройств линейки Galaxy. 
+// При скролле появляются левый блок двигается вниз, правый - вверх. Таким образом появляются новые материалы.
 $(document).ready(function(){
 	$('.slider').slick({
 		centerMode: true,
@@ -57,6 +63,12 @@ $(document).ready(function(){
 			if ($(this).scrollTop() >= 800) {
 				$('.timeline-item:nth-child(10)').css('opacity', '1');
 				$('.timeline-item:nth-child(10)').css('right', 'calc(-50% - 13px)');
+			}
+			if ($(this).scrollTop() >= 2100) {
+				$('#technology>.container>.blocks>.col:nth-child(1)').css('opacity', '1');
+				$('#technology>.container>.blocks>.col:nth-child(1)').css('top', '0');
+				$('#technology>.container>.blocks>.col:nth-child(2)').css('opacity', '1');
+				$('#technology>.container>.blocks>.col:nth-child(2)').css('bottom', '0');
 			}
 		}
 	});
