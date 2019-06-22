bc69dca181923bcbbc0fd0707cc543acef64b241
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-06-22 14:33:07 +0300

    Add files via upload

--- samsung.loc/assets/js/main.js
+++ samsung.loc/assets/js/main.js
@@ -13,4 +13,51 @@ $(document).ready(function(){
 		prevArrow: $('#footer-left-arrow'),
 		nextArrow: $('#footer-right-arrow')
 	});
+	$(window).scroll(function(){
+
+		var st = $(this).scrollTop();
+
+		if($(window).width() >= 1196){
+			if ($(this).scrollTop() >= 80) {
+				$('.timeline-item:nth-child(1)').css('opacity', '1');
+				$('.timeline-item:nth-child(1)').css('left', '0');
+			}
+			if ($(this).scrollTop() >= 160) {
+				$('.timeline-item:nth-child(2)').css('opacity', '1');
+				$('.timeline-item:nth-child(2)').css('right', 'calc(-50% - 13px)');
+			}
+			if ($(this).scrollTop() >= 240) {
+				$('.timeline-item:nth-child(3)').css('opacity', '1');
+				$('.timeline-item:nth-child(3)').css('left', '0');
+			}
+			if ($(this).scrollTop() >= 320) {
+				$('.timeline-item:nth-child(4)').css('opacity', '1');
+				$('.timeline-item:nth-child(4)').css('right', 'calc(-50% - 13px)');
+			}
+			if ($(this).scrollTop() >= 400) {
+				$('.timeline-item:nth-child(5)').css('opacity', '1');
+				$('.timeline-item:nth-child(5)').css('left', '0');
+			}
+			if ($(this).scrollTop() >= 480) {
+				$('.timeline-item:nth-child(6)').css('opacity', '1');
+				$('.timeline-item:nth-child(6)').css('right', 'calc(-50% - 13px)');
+			}
+			if ($(this).scrollTop() >= 560) {
+				$('.timeline-item:nth-child(7)').css('opacity', '1');
+				$('.timeline-item:nth-child(7)').css('left', '0');
+			}
+			if ($(this).scrollTop() >= 640) {
+				$('.timeline-item:nth-child(8)').css('opacity', '1');
+				$('.timeline-item:nth-child(8)').css('right', 'calc(-50% - 13px)');
+			}
+			if ($(this).scrollTop() >= 720) {
+				$('.timeline-item:nth-child(9)').css('opacity', '1');
+				$('.timeline-item:nth-child(9)').css('left', '0');
+			}
+			if ($(this).scrollTop() >= 800) {
+				$('.timeline-item:nth-child(10)').css('opacity', '1');
+				$('.timeline-item:nth-child(10)').css('right', 'calc(-50% - 13px)');
+			}
+		}
+	});
 });
