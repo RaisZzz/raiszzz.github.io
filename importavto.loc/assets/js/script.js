c171bee7f0c2ac4004bd2c1836b77042c5acecdf
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-07-12 22:37:43 +0300

    Add files via upload

--- importavto.loc/assets/js/script.js
+++ importavto.loc/assets/js/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
 	var cont = $('.container').width();
+	var cat = $('.catalog').width();
 	var dw = document.documentElement.clientWidth;
 	var dwc = (dw - cont)/2;
-	$('.slide>.text').css({'left':dwc+'px'});
+	$('.slide>.text').css({'left':(dwc + cat)+'px'});
 	$('.catalog').css({'left':'-'+dwc+'px'});
 	$('.catalog').css({'padding-left':dwc+'px'});
+	$('.container-2').css({'width':(cont - cat*2)+'px'});
+	$('#vert-h').css({'left':(dwc)+'px'});
 
 	$('.slider').slick({
 		arrows: true,
@@ -12,6 +15,6 @@ $(document).ready(function(){
 		prevArrow: $('.prev'),
 		nextArrow: $('.next')
 	});
-	$('.slick-dots').css({'right':dwc+'px'});
-	$('.arrows').css({'right':dwc+'px'});
+	$('.slick-dots').css({'right':(dwc + cat)+'px'});
+	$('.arrows').css({'right':(dwc + cat)+'px'});
 });
