d3766c89987dfc66959cf4c03e917231d61dcf42
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2019-08-20 15:18:25 +0300

    Add files via upload

--- importavto.loc/assets/js/script.js
+++ importavto.loc/assets/js/script.js
@@ -17,4 +17,32 @@ $(document).ready(function(){
 	});
 	$('.slick-dots').css({'right':(dwc + cat)+'px'});
 	$('.arrows').css({'right':(dwc + cat)+'px'});
+
+	$('.dark').on('click', function(){
+		$('.dark').css({'display':'none'});
+		$('.light').css({'display':'block'});
+		$('body').css({'background':'#2d3436'});
+		$('#header').css({'background':'#2d3436'});
+		$('#skills').css({'background':'#636e72'});
+		$('.menu').css({'border':'none'});
+		$('.cart').css({'border':'none'});
+		$('.like').css({'border':'none'});
+		$('p').css({'color':'#ffffff'});
+		$('a').css({'color':'#ffffff'});
+		$('h2').css({'color':'#ffffff'});
+		$('.first>p').css({'color':'#ffffff !important'});
+		$('.auto>.row>a').css({'background':'#b2bec3'});
+	});
+	$('.light').on('click', function(){
+		$('.light').css({'display':'none'});
+		$('.dark').css({'display':'block'});
+		$('body').css({'background':'#fff'});
+		$('#header').css({'background':'#fff'});
+		$('#skills').css({'background':'#f5f5f5'});
+		$('p').css({'color':'#202020'});
+		$('a').css({'color':'#202020'});
+		$('h2').css({'color':'#202020'});
+		$('.first>p').css({'color':'#535353 !important'});
+		$('.auto>.row>a').css({'background':'#b2bec3'});
+	});
 });
