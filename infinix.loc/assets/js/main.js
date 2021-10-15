5b6d3882c89f82eb68ee7328d77a5894f6270e09
RaisZzz <40802055+RaisZzz@users.noreply.github.com>
2021-10-15 14:51:24 +0300

    Add files via upload

--- infinix.loc/assets/js/main.js
+++ infinix.loc/assets/js/main.js
@@ -1,10 +1,10 @@
-$4.onInit("jQuery",function($){
+$4.onInit("jQuery Slick",function($){
 	$(document).ready(function() {
 		$('.infinix-slider').slick({
 			autoplay: true,
-			autoplaySpeed: 5000,
-			// prevArrow: $('.huawei-prev-arrow'),
-			// nextArrow: $('.huawei-next-arrow'),
+			autoplaySpeed: 2000,
+			prevArrow: $('.infinix-arrow-prev'),
+			nextArrow: $('.infinix-arrow-next'),
 			dots: true
 		});
 	});
